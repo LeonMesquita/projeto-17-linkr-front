3c5a69b6a1afd0e1067bdd9df42b7bf3d4c8e2b4
Luan Severo <luansevero@icloud.com>
2022-08-12 03:58:26 -0300

    [Feat] 2nd Version from Post Skeleton Loading (Have to much bugs)

--- src/components/postCards/Skeletons/PostSkeleton.js
+++ src/components/postCards/Skeletons/PostSkeleton.js
@@ -10,19 +10,29 @@ export default function PostSkeleton() {
     return (
         <CardContainer className="post">
             <PostContentSide className="post">
-                <SkeletonTheme baseColor="#171717" highlightColor="#272727" width="100%" height="23px" >
+                <SkeletonTheme baseColor="#171717" highlightColor="#272727" width="100%" height="100%" borderRadius="25px">
                     <ProfilePicLoading>
-                        <p><Skeleton count={1} duration={2} height="100%" width="100%" borderRadius="25px"/></p>
+                        <p><Skeleton count={1} duration={1.2}  /></p>
                     </ProfilePicLoading>
                 </SkeletonTheme>
             </PostContentSide>
             <PostSide>
-            <SkeletonTheme baseColor="#171717" highlightColor="#272727" width="100%" height="23px" >
+            <SkeletonTheme baseColor="#171717" highlightColor="#272727" width="100%" height="100%" duration={1.2}>
                     <PublishLoading>
-                        <p><Skeleton  count={1} height="100%" width="100%" duration={2}/></p>
-                        <p><Skeleton  count={1} height="100%" width="100%" duration={2}/></p>
-                        <p><Skeleton  count={1} height="100%" width="100%" duration={2}/></p>
-                        <p><Skeleton  count={1} height="100%" width="100%" duration={2} borderRadius="12px"/></p>
+                        <p><Skeleton  count={1} /></p>
+                        <p><Skeleton  count={1} /></p>
+                        <p><Skeleton  count={1} /></p>
+                        <UrlBox>
+                            <UrlBoxTextSide>
+                                <span><Skeleton  count={1} /></span>
+                                <span><Skeleton  count={1} /></span>
+                                <span><Skeleton  count={1} /></span>
+                            </UrlBoxTextSide>
+                            <UrlPNGSide>
+                                <h1><Skeleton  count={1} borderRadius="0px 12px 13px 0px" /></h1>
+                            </UrlPNGSide>
+                        </UrlBox>
+                        
 
                     </PublishLoading>
                 </SkeletonTheme>
@@ -37,6 +47,44 @@ const ProfilePicLoading = styled.div`
         height:50px;
     }
 `
+
+
+
+
+const UrlBox = styled.div`
+    display:flex;
+    border: 1px solid #272727;
+    height: 157px;
+    width: 100%;
+    margin-top: 10px;
+    border-radius: 11px;
+`
+
+const UrlBoxTextSide = styled.div`
+    display:flex;
+    width: 350px;
+    padding: 24px 20px;
+    flex-direction: column;
+    & span:nth-child(1){
+        height: 19px;
+        margin-bottom: 5px;
+    }
+    & span:nth-child(2){
+        height: 50px;
+        margin-bottom: 5px;
+    }
+    & span:nth-child(3){
+        height: 13px;
+        margin-top: 13px;
+    }
+`
+const UrlPNGSide = styled.div`
+    & h1:nth-child(1){
+        width: 155px;
+        height: 152px;
+    }
+`
+
 const PublishLoading = styled.div`
     & p{border-radius: 5px;}
     & p:nth-child(1){
@@ -50,15 +98,16 @@ const PublishLoading = styled.div`
     & p:nth-child(3){
         height: 30px;
     }
-    & p:nth-child(4){
-        border: 1px solid #272727;
-        height: 150px;
-        margin-top: 10px;
-        border-radius: 11px;
+    @media screen and (max-width: 645px){
+        p,h1,span{
+            width:100%
+        }
     }
 
-
     @media screen and (max-width: 431px){
+        p,h1,span{
+            width:100%
+        }
         & p:nth-child(1){
             height: 20px;
             margin-bottom: 7px;
@@ -72,6 +121,9 @@ const PublishLoading = styled.div`
                 width: 40px;
                 heigth: 40px;
             }
+        }
+        ${UrlBoxTextSide}{
+
         }
     }
 `
