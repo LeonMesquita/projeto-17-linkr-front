af1caa1569d98b04cb5d0b34663e2738588fb12a
Luan Severo <luansevero@icloud.com>
2022-08-15 00:15:17 -0300

    [Fix] Wrong Style in PostCard

--- src/components/postCards/PostCard.js
+++ src/components/postCards/PostCard.js
@@ -3,150 +3,198 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import styled from "styled-components";
-import { CardContainer, PostContentSide, PostSide } from  "../style.js";
-import {IoIosHeartEmpty, IoIosHeart} from "react-icons/io";
+import { CardContainer, PostContentSide, PostSide } from "../style.js";
+import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io";
 import TokenContext from "../../contexts/TokenContext.js";
 import UserContext from "../../contexts/UserContext.js";
 
 import { ReactTagify } from "react-tagify";
+import { IoMdTrash } from 'react-icons/io'
+import { TiPencil } from 'react-icons/ti'
 
-export default function PostCard({author,author_pic,description, postUrl, onclick, postId, userId}){
-    if(!userId){
+export default function PostCard({ author, author_pic, description, postUrl, onclick, postId, userId }) {
+    console.log(userId)
+    if (!userId) {
         userId = -1;
     }
     
     const navigate = useNavigate();
-    
-    const { token, setToken, authorization } = useContext(TokenContext);
+
+    const { authorization } = useContext(TokenContext);
     const { url, user, setUser } = useContext(UserContext);
     const [isFavorite, setIsFavorite] = useState(false);
     const [numberOfFavorites, setNumberOfFavorites] = useState(0);
     const urldata = {
         postUrl: "",
         title: "",
-        siteName:"",
-        description:"",
-        mediaType:"",
-        contentType:"",
-        images:[],
-        favicons:[],
-        videos:[]
+        siteName: "",
+        description: "",
+        mediaType: "",
+        contentType: "",
+        images: [],
+        favicons: [],
+        videos: []
     }
+    const linkrUser = JSON.parse(localStorage.getItem("linkrUser"));
+    const linkrUserToken =  linkrUser.token;
+    const linkrUserId = linkrUser.id ;
 
-    const [ data, setData ] = useState(urldata);
+    const [data, setData] = useState(urldata);
 
-    let body={
+    let body = {
         "url": postUrl
     }
 
-    async function getFavorites(postId, userId){
+    async function getFavorites(postId, userId) {
 
-        try{
-            const promise =  await axios.get(`${url}/posts/favorite/${postId}/${userId}`);
+        try {
+            const promise = await axios.get(`${url}/posts/favorite/${postId}/${userId}`);
             setNumberOfFavorites(promise.data.favoriteQuantity);
             setIsFavorite(promise.data.isFavorite);
 
-        }catch(e){
+        } catch (e) {
 
         }
     }
 
     useEffect(() => {
-        const promise = axios.post(`${url}/urls`,body); //`https://linkr-back-api.herokuapp.com/urls`
-        promise.then((res)=>{
-            
+        const promise = axios.post(`${url}/urls`, body); //`https://linkr-back-api.herokuapp.com/urls`
+        promise.then((res) => {
+
             setData(res.data);
 
-           // console.log(data);
+            // console.log(data);
         });
         promise.catch(() => {
 
         });
 
         getFavorites(postId, userId);
-    },[]);
+    }, []);
 
-    async function onClickFavorite(){
-        try{
+    async function onClickFavorite() {
+        try {
             await axios.post(`${url}/posts/favorite`,
-            {postId, userId},
-            authorization);
+                { postId, userId },
+                authorization);
 
             setIsFavorite(!isFavorite);
             getFavorites(postId, userId);
-        }catch(e){
+        } catch (e) {
 
         }
     }
 
 
-    async function removeFavorite(){
-        try{
+    async function removeFavorite() {
+        try {
             await axios.delete(`${url}/posts/favorite/${postId}/${userId}`,
-            
-            authorization);
+
+                authorization);
             setIsFavorite(!isFavorite);
             getFavorites(postId, userId);
-        }catch(e){
+        } catch (e) {
             console.log(e)
 
         }
     }
-    const handleNavigate = (tag) => navigate(`/hashtag/${tag.slice(1)}`);
-    const tagStyle = {fontWeight: "700", fontSize: "17px", lineHeight: "20px", color: "#FFFFFF"};
-    return(
-        <>
-        {data ? (
-                <CardContainer className="post">
-                <PostContentSide>
-                    <img src={author_pic} alt="user" />
-                </PostContentSide>
-                <PostSide>
-                    <PostInfos>
-                        <p onClick={onclick}>{author}</p>
-                        <ReactTagify
-                            tagStyle={tagStyle}
-                            detectLinks={false}
-                            tagClicked={(tag) => handleNavigate(tag)}
 
-                        >
-                            <span>{description}</span>
-                        </ReactTagify>
-                        
-                        <a style={{display: "table-cell"}} href = {url} target = "_blank" 
-                            rel = "noopener noreferrer"><UrlContainer>
-                            <UrlDescriptionSide>
-                                <h1>{data.title}</h1>
-                                <span>{data.description}</span>
-                                <h2>{postUrl}</h2>
-                            </UrlDescriptionSide>
-                            <UrlImageSide>
-                                <img src={data.favicons[0]} alt={data.title}></img>
-                            </UrlImageSide>
-                            </UrlContainer></a>
-                    </PostInfos>
-                </PostSide>
-                <FavoriteDiv iconColor={isFavorite ? '#AC0C00' : 'white'}>
-                    {isFavorite ? <IoIosHeart onClick={removeFavorite}/> : <IoIosHeartEmpty  onClick={onClickFavorite}/>}
-                    
-                    <h6>{numberOfFavorites} Likes</h6>
-                </FavoriteDiv>
-            </CardContainer>
-            )
-            : (<h1>Loading . . . </h1>)
-        }
+    const handleDeletePost = () => {
+
+    }
+
+    const EditPost = () => {
+
+    }
+
+    const handleNavigate = (tag) => navigate(`/hashtag/${tag.slice(1)}`);
+
+
+    const tagStyle = { fontWeight: "700", fontSize: "17px", lineHeight: "20px", color: "#FFFFFF" };
+    return (
+        <>
+            {
+                data ? (
+                    <CardContainer className="post">
+                        <PostContentSide>
+                            <img src="https://pbs.twimg.com/media/FP3A-hnWQAMTQuK?format=jpg" alt="user" />
+                            <LikeContainer iconColor={isFavorite ? 'AC0C00' : "FFFFFF"}>
+                                {isFavorite ? <IoIosHeart onClick={removeFavorite} /> : <IoIosHeartEmpty onClick={onClickFavorite} />}
+
+                                <h6>{numberOfFavorites} Likes</h6>
+                            </LikeContainer>
+                        </PostContentSide>
+                        <PostSide>
+                            <PostInfos>
+                                <PostOwnerContainer>
+                                    {/* <p onClick={onclick}>{author}</p> */}
+                                    <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                                    <InteractionContainer className={linkrUserId === userId ? "" : "notAuthorPost"}>
+                                        <TiPencil />
+                                        <IoMdTrash />
+                                    </InteractionContainer>
+                                </PostOwnerContainer>
+                                <ReactTagify
+                                    tagStyle={tagStyle}
+                                    detectLinks={false}
+                                    tagClicked={(tag) => handleNavigate(tag)}
+                                >
+                                    <span>{description}</span>
+
+                                </ReactTagify>
+
+                                <LinkPreview
+                                    href={url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <PreviewDescription>
+                                        <h1>{data.title}</h1>
+                                        <span>{data.description}</span>
+                                        <h2>{postUrl}</h2>
+                                    </PreviewDescription>
+                                    <img src="https://pbs.twimg.com/media/FP3A-hnWQAMTQuK?format=jpg" alt={data.title}></img>
+                                </LinkPreview>
+                            </PostInfos>
+                        </PostSide>
+                    </CardContainer>
+                )
+                    : <></>
+            }
+
         </>
     )
 };
+
+const LikeContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items:center;
+    svg{
+        width: 25px;
+        height: 25px;
+        color: #${props => props.iconColor};
+        margin-bottom: 10px;
+    }
+    h6{
+        font-family: 'Lato';
+        font-weight: 400;
+        font-size: 11px;
+        line-height: 13px;
+        color: #FFFFFF;
+    }
+`
 
 const PostInfos = styled.div`
 
     h1,span{font-weight: 400;}
     h1,p{
-        margin-bottom: 7px;
         font-size: 19px;
         line-height: 23px;
         color: #FFFFFF;
+    }
+    h1{
+        margin-bottom: 7px;
     }
     span{
         font-size: 17px;
@@ -173,18 +221,37 @@ const PostInfos = styled.div`
     }
 `
 
-const UrlContainer = styled.div`
+const PostOwnerContainer = styled.div`
+    min-height: 23px;
     display:flex;
-    width: 100%;
-    height: 155px;
-    margin-top: 10px;
-    border: 1px solid #4D4D4D;
-    border-radius: 11px;
+    margin-bottom: 7px;
+    justify-content: space-between;
+    p{
+        word-break: break-all;
+    }
 `
 
-const UrlDescriptionSide = styled.div`
+const InteractionContainer = styled.div`
+    height: 100%;
+    display:flex;
+    align-items:center;
+    svg{
+        width: 20px;
+        height: 20px;
+        color: #FFFFFF;
+        margin-left: 5px;
+    }
+    &.notAuthorPost{
+        display:none;
+    }
+`
+
+
+
+const PreviewDescription = styled.div`
     display:flex;
     flex-direction: column;
+    width: 100%;
     justify-content: space-between;
     padding: 24px 20px;
     h1,span,h2{font-weight: 400;}
@@ -204,41 +271,53 @@ const UrlDescriptionSide = styled.div`
         line-height: 13px;
         color: #CECECE;
     }
-    @media screen and (max-width: 431px){
-        h1{
-            font-size: 11px;
-            line-height: 13px;
-            color: #CECECE;
-        }
-        span, h2{
-            font-size: 9px;
-            line-height: 11px;
-            color: #9B9595;
-        }
-        h2{color: #CECECE;}
-    }
 `
 
-const UrlImageSide = styled.div`
-   img{
-    width: 153.44px;
-    height: 153.44px;
-   }
-`
-
-const FavoriteDiv = styled.div`
-    position: absolute;
-    top: 90px;
-    left: 27px;
-    text-align: center;
-    color: ${props => props.iconColor};
-    font-size: 20px;
-
-    h6{
-        font-family: 'Lato';
-        font-weight: 400;
-        font-size: 11px;
-        line-height: 13px;
-        color: #FFFFFF;
+const LinkPreview = styled.a`
+    display:flex;
+    align-items:center;
+    width: 100%;
+    height: 155px;
+    margin-top: 10px;
+    border: 1px solid #4D4D4D;
+    border-radius: 11px;
+    img{
+        float: right;
+        width: 155px;
+        height: 100%;
+        border-radius: 0px 10px 10px 0px;
     }
+    @media screen and (max-width: 611px){
+        ${PreviewDescription}{
+            width: 100%;
+            h1{
+                font-size: 11px;
+                line-height: 13px;
+                color: #CECECE;
+            }
+            span, h2{
+                font-size: 9px;
+                line-height: 11px;
+                color: #9B9595;
+            }
+            h2{color: #CECECE;}
+        }
+        ${LikeContainer}{
+            svg{
+                width: 17px;
+                height: 17px;
+            }
+            h6{
+                font-size: 9px;
+                line-height: 11px;
+            }
+        }
+    }
+    @media screen and (max-width: 430px){
+        height: 115px;
+        img{
+            width: 95px;
+        }
+    }
+
 `
